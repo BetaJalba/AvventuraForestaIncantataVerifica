364f64555de2d574905dd85e5f9b4b34ac32e507
10934163 <10934163@itisrossi.vi.it>
2024-11-27 13:27:28 +0100

    Fix

--- AvventuraForestaIncantataVerifica/CGiocatore.cs
+++ AvventuraForestaIncantataVerifica/CGiocatore.cs
@@ -14,7 +14,8 @@ namespace AvventuraForestaIncantataVerifica
         bool                        canThrow;
         int                         mappaIndex,
                                     oldMappaIndex, // risolve un problema molto specifica
-                                    pId;
+                                    pId,
+                                    effetto;
         CCasella[]                  mappa;
         public event EventHandler   OnShouldThrow;
         public event Action<string> OnWin;
@@ -25,6 +26,7 @@ namespace AvventuraForestaIncantataVerifica
             mappaIndex = 0;
             pId = count;
             count++;
+            effetto = -1;
             this.mappa = mappa;
             for (int i = 0; i < this.mappa.Length; i++) 
             {
@@ -32,11 +34,15 @@ namespace AvventuraForestaIncantataVerifica
                 {
                     if (sender is CCasellaRagnatela & e.Casella == mappaIndex && mappaIndex != oldMappaIndex) 
                     {
+                        effetto = 0;
                         saltaTurno();
                     }
                         
-                    else if (sender is CCasellaAlbero && e.Casella == mappaIndex && mappaIndex != oldMappaIndex)
+                    else if (sender is CCasellaAlbero && e.Casella == mappaIndex && mappaIndex != oldMappaIndex) 
+                    {
                         ritiraDado();
+                        effetto = 1;
+                    }
                 };
             }
         }
@@ -54,12 +60,21 @@ namespace AvventuraForestaIncantataVerifica
                     return null;
                 }
 
-                if (mappa[mappaIndex].Effetto() > 0)
-                    bonusString = $"\nBonus di {mappa[mappaIndex].Effetto()} passi per il giocatore {pId} per essere atterrato sulla casella {mappaIndex}!";
-                else if (mappa[mappaIndex].Effetto() < 0)
-                    bonusString = $"\nIl giocatore {pId} perde {Math.Abs(mappa[mappaIndex].Effetto())} posizioni per essere atterrato sulla casella {mappaIndex}!";
+                int extra = mappa[mappaIndex].Effetto(); // lo richiama solo una volta
 
-                mappaIndex += mappa[mappaIndex].Effetto();
+                if (extra > 0)
+                    bonusString = $"\nBonus di {extra} passi per il giocatore {pId} per essere atterrato sulla casella {mappaIndex}!";
+                else if (extra < 0)
+                    bonusString = $"\nIl giocatore {pId} perde {Math.Abs(extra)} posizioni per essere atterrato sulla casella {mappaIndex}!";
+                else if (effetto == 0)
+                    bonusString = $"\nIl giocatore {pId} non potrà tirare il prossimo turno!";
+                else if (effetto == 1)
+                    bonusString = $"\nIl giocatore {pId} ha diritto a lanciare di nuovo il dado!";
+                
+                if (effetto != -1)
+                    effetto = -1;
+
+                mappaIndex += extra;
 
                 return $"Giocatore {pId} avanza fino alla {mappa[mappaIndex].Nome} in posizione {mappaIndex}. {bonusString}";
             }
